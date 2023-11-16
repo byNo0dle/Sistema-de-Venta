6e40c03f8e9aee7b37cedc77ba109fd51f161c72
Nick <nicksaurinocruz@gmail.com>
2023-11-16 16:38:33 -0500

    Prueba unitaria de logica de negocio productService

--- src/main/java/com/ufoStyle/salesystem/domain/services/ProductServiceImpl.java
+++ src/main/java/com/ufoStyle/salesystem/domain/services/ProductServiceImpl.java
@@ -3,15 +3,18 @@ package com.ufoStyle.salesystem.domain.services;
 import com.ufoStyle.salesystem.domain.entities.Category;
 import com.ufoStyle.salesystem.domain.entities.Product;
 import com.ufoStyle.salesystem.domain.repositories.ProductRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.Date;
 import java.util.List;
 
+@Service
+@RequiredArgsConstructor
 public class ProductServiceImpl implements ProductService{
 
-    @Autowired
-    private ProductRepository productRepository;
+    private final ProductRepository productRepository;
 
     @Override
     public List<Product> listAllProduct() {
---
+++ src/test/java/com/ufoStyle/salesystem/domain/services/ProductServiceTest.java
@@ -0,0 +1,53 @@
+package com.ufoStyle.salesystem.domain.services;
+
+import com.ufoStyle.salesystem.domain.entities.Category;
+import com.ufoStyle.salesystem.domain.entities.Product;
+import com.ufoStyle.salesystem.domain.repositories.ProductRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ProductServiceTest {
+
+    @Mock
+    private ProductRepository productRepository;
+    private ProductService productService;
+
+    @BeforeEach
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+        productService = new ProductServiceImpl(productRepository);
+        Product computer = Product.builder()
+                .id(1L)
+                .name("computer")
+                .category(Category.builder().build())
+                .price(Double.parseDouble("12.5"))
+                .stock(Double.parseDouble("5"))
+                .build();
+        Mockito.when(productRepository.findById(1L))
+                .thenReturn(Optional.of(computer));
+        Mockito.when(productRepository.save(computer))
+                .thenReturn(computer);
+    }
+
+    @Test
+    public void whenValidGetId_ThenReturnProduct() {
+        Product found = productService.getProduct(1L);
+        Assertions.assertThat(found.getName()).isEqualTo("computer");
+    }
+
+    @Test
+    public void whenValidUpdateStock_ThenReturnNewStock() {
+        Product newStock = productService.updateStock(1L, Double.parseDouble("8"));
+        Assertions.assertThat(newStock.getStock()).isEqualTo(13);
+    }
+}
