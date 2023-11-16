4a19eb325405680e68c90c8b06439e4d2cca05b0
Nick <nicksaurinocruz@gmail.com>
2023-11-16 15:41:12 -0500

    Se agrego la clase y el interfaz de de product service

---
+++ src/main/java/com/ufoStyle/salesystem/domain/services/ProductService.java
@@ -0,0 +1,16 @@
+package com.ufoStyle.salesystem.domain.services;
+
+import com.ufoStyle.salesystem.domain.entities.Category;
+import com.ufoStyle.salesystem.domain.entities.Product;
+
+import java.util.List;
+
+public interface ProductService {
+    public List<Product> listAllProduct();
+    public Product getProduct(Long id);
+    public Product createProduct(Product product);
+    public Product updateProduct(Product product);
+    public Product deleteProduct(Long id);
+    public List<Product> findByCategory(Category category);
+    public Product updateStock(Long id, Double quantity);
+}
