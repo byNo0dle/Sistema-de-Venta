0088583020f61b34241191b4747be2b96c142592
Nick <nicksaurinocruz@gmail.com>
2023-11-17 13:34:15 -0500

    Tratando de solucionar el error en H2 DB y Mockito

--- src/main/java/com/ufoStyle/salesystem/SalesystemApplication.java
+++ src/main/java/com/ufoStyle/salesystem/SalesystemApplication.java
@@ -2,8 +2,9 @@ package com.ufoStyle.salesystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class SalesystemApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/ufoStyle/salesystem/domain/entities/Category.java
+++ src/main/java/com/ufoStyle/salesystem/domain/entities/Category.java
@@ -5,7 +5,7 @@ import lombok.*;
 import javax.persistence.*;
 
 @Entity
-@Table(name = "tbl_categories")
+@Table(name = "TBL_CATEGORIES")
 @Getter
 @Setter
 @Data
