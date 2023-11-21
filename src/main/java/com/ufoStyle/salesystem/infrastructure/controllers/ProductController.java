6bb81db68bab77776e804713718c09f17fae1b68
Nick <nicksaurinocruz@gmail.com>
2023-11-21 10:25:25 -0500

    Se creo un exception de createProduct

--- src/main/java/com/ufoStyle/salesystem/infrastructure/controllers/ProductController.java
+++ src/main/java/com/ufoStyle/salesystem/infrastructure/controllers/ProductController.java
@@ -1,15 +1,24 @@
 package com.ufoStyle.salesystem.infrastructure.controllers;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ufoStyle.salesystem.domain.entities.Category;
 import com.ufoStyle.salesystem.domain.entities.Product;
+import com.ufoStyle.salesystem.domain.events.ErrorMessage;
 import com.ufoStyle.salesystem.domain.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
+import javax.validation.Valid;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/api/products")
@@ -46,7 +55,11 @@ public class ProductController {
     }
 
     @PostMapping
-    public ResponseEntity<Product> createProduct(@RequestBody Product product) {
+    public ResponseEntity<Product> createProduct(
+            @Valid @RequestBody Product product, BindingResult result) {
+        if (result.hasErrors()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, this.formatMessage(result));
+        }
         Product createProduct = productService.createProduct(product);
         return ResponseEntity.status(HttpStatus.CREATED).body(createProduct);
     }
@@ -80,5 +93,26 @@ public class ProductController {
             return ResponseEntity.notFound().build();
         }
         return ResponseEntity.ok(product);
+    }
+
+    private String formatMessage(BindingResult result) {
+        List<Map<String, String>> errors = result.getFieldErrors().stream()
+                .map(err -> {
+                    Map<String, String> error = new HashMap<>();
+                    error.put(err.getField(), err.getDefaultMessage());
+                    return  error;
+                })
+                .collect(Collectors.toList());
+        ErrorMessage errorMessage = ErrorMessage.builder()
+                .code("01")
+                .messages(errors).build();
+        ObjectMapper mapper = new ObjectMapper();
+        String jsonString = "";
+        try {
+            jsonString = mapper.writeValueAsString(errorMessage);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return jsonString;
     }
 }
